12551c65b09725a466344f9d4c57edd42f3513e6
Bernard <bernardjohnson2012@gmail.com>
2015-01-17 09:47:34 -0200

    primeiro commit

---
+++ Jsoftware.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jsoftware.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestJsoftware
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Jsoftware.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jsoftware.LojaVirtual.Web.MvcApplication" Language="C#" %>
